3a047106997c0ccad66f078667b37813d71e9c10
Yura Sokolov <funny.falcon@gmail.com>
2023-08-14 17:36:40 +0300

    simplify a lot

--- lib/fupeg/parser.rb
+++ lib/fupeg/parser.rb
@@ -4,153 +4,25 @@ require "strscan"
 
 module FuPeg
   class Parser
-    class PositionSearcher
-      Position = Struct.new(:lineno, :colno, :line, :char)
-
-      def initialize(str)
-        @str = str
-        @line_starts = []
-        pos = 0
-        while pos = str.index("\n", pos)
-          pos += 1
-          @line_starts << @pos
-        end
-        @line_starts << @str.size
-      end
-
-      def position(pos)
-        lineno = @line_starts.bsearch_index { |x| x > pos }
-        case lineno
-        when nil
-          raise "Position #{pos} is larger than string size #{@str.size}"
-        when 0
-          line_start = 0
-          column = pos
-        else
-          line_start = @line_starts[lineno - 1]
-          column = pos - line_start
-        end
-        line = @str[line_start...@line_starts[lineno]]
-        char = @str[@pos]
-        Position.new(lineno + 1, column + 1, line, char)
-      end
-    end
-
-    class Memoizer
-      class Item
-        def initialize(rule, args, nxt)
-          @rule = rule
-          @args = args
-          @result = nil
-          @state = :new
-          @next = nxt
-        end
-
-        attr_reader :rule, :args, :result, :_next
-
-        def find(rule, args)
-          i = @self
-          i = i._next until i.nil? || i.match?(rule, args)
-          i
-        end
-
-        def match?(rule, args)
-          @rule == rule && @args == args
-        end
-
-        def state
-          case @state
-          when Number
-            :success
-          else
-            @state
-          end
-        end
-
-        def delay!
-          @state = :delay
-        end
-
-        def left_recursive!
-          @state = :left_recursive
-        end
-
-        def set(endpos, result)
-          @state = endpos
-          @result = result
-        end
-
-        def fail!
-          @state = :fail
-        end
-      end
-
-      def initialize
-        @map = {}
-      end
-
-      def [](pos, rule, args = nil)
-        list = @map[pos]
-        item = list&.find(rule, args)
-        item || (@map[pos] = Item.new(rule, args, list))
-      end
-    end
-
-    class ChoiceStack
-      ChoicePoint = Struct.new(:pos, :cut, :sno)
-
-      def initialize(scan)
-        @scan = scan
-        @stack = []
-        @cur = nil
-        @sno = 0
-      end
-
-      def with
-        point = ChoicePoint.new(@scan.pos, false, (@sno += 1))
-        @stack << point
-        prev, @cur = @cur, point
-        yield point
-      ensure
-        @stack.pop if @stack.last == point
-        if @stack.last && (@stack.last.sno >= point.sno || @stack.last.pos > point.pos)
-          raise "ChoiceStack logic error: #{@stack.last} > #{point}"
-        end
-        @cur = prev
-      end
-
-      def cut!
-        @cur.cut = true
-        @stack.pop while @stack.last&.cut
-        @stack.empty?
-      end
-    end
-
     def initialize(str, pos = 0)
-      reset(str, pos)
+      reset!(str, pos)
     end
 
-    def reset(str, pos = 0)
-      @str = str
-      @str_size = str.size
-      @pos_searcher = PositionSearcher.new(str)
-      @scan = StringScanner.new(str)
-      @scan.pos = pos
-      @memoize = Memoizer.new
-      @current_rule = nil
+    def reset!(str = nil, pos = nil)
+      if str
+        @str = str.dup
+        @str_size = str.size
+        init_line_ends
+        @scan = StringScanner.new(str)
+      end
+      if pos
+        @scan.pos = pos
+      end
       @failed = nil
-      @result = nil
-
-      @choice_stack = ChoiceStack.new(@scan)
+      @cut = CutPoint.new
     end
 
-    attr_reader :result
     attr_reader :failed
-
-    def parse(rule = :root)
-      @failed = nil
-      match_rule(rule)
-    end
 
     def bytepos
       @scan.pos
@@ -160,135 +32,190 @@ module FuPeg
       @str_size - @str.byteslice(@scan.pos..).size
     end
 
-    Fail = Struct.new(:rule, :pos)
+    Fail = Struct.new(:stack, :pos, :bytepos)
 
-    def set_failed(rule)
-      if !@failed || @scan.pos > @failed.pos
-        @failed = Fail.new(rule, @scan.pos)
+    def fail!(skip = 2)
+      if !@failed || bytepos > @failed.bytepos
+        stack = caller_locations(skip)
+        stack.delete_if do |loc|
+          if loc.path == __FILE__
+            loc.label =~ /\b(_bt|each|block)\b/
+          end
+        end
+        pos = position_for_charpos(charpos)
+        @failed = Fail.new(stack, pos, bytepos)
       end
+      nil
     end
 
-    def match_fail
-      false
+    def report_failed(out)
+      pos = @failed.pos
+      out << "Failed at #{pos.lineno}:#{pos.colno} :\n"
+      out << pos.line + "\n"
+      out << (" " * (pos.colno - 1) + "^\n")
+      out << "Call stack:\n"
+      @failed.stack.each do |loc|
+        out << "#{loc.path}:#{loc.lineno} in #{loc.label}\n"
+      end
+      out
     end
 
-    def match_empty
-      true
-    end
-
-    def match_dot
-      @scan.skip(/./) && true
+    def dot
+      @scan.scan(/./m) || fail!
     end
 
     begin
       StringScanner.new("x").skip("x")
-      def match_lit(reg_or_str)
-        @scan.skip(reg_or_str) && true
+      def lit(reg_or_str)
+        @scan.scan(reg_or_str) || fail!
       end
     rescue
-      def match_lit(reg_or_str)
+      def lit(reg_or_str)
         if String === reg_or_str
           @__match_lit_cache ||= Hash.new { |h, s| h[s] = Regexp.new(Regexp.escape(s)) }
           reg_or_str = @__match_lit_cache[reg_or_str]
         end
-        @scan.skip(reg_or_str) && true
+        @scan.scan(reg_or_str) || fail!
       end
     end
 
-    def match_rule(rule)
-      save_pos = @scan.pos
-      @result = nil
-      name = nil
-      res = case rule
-      when Symbol
-        name = rule
-        __send__(rule)
-      when Array
-        name = rule[0]
-        __send__(*rule)
-      else
-        rule.call
-      end
-      unless res
-        set_failed(name) if name
-        @scan.pos = save_pos
-      end
-      res
+    def seq(*args, &block)
+      _bt(&block)
     end
 
-    def assert_pos(pos)
-      if pos != @scan.pos
-        raise "Fail didn't return pos from #{@scan.pos} to #{point.pos}"
-      end
+    def opt(&block)
+      _rewind(nil, @failed, _bt(&block) || true)
     end
 
-    def match_choices(choices)
-      @choice_stack.with do |point|
-        choices.each do |choice|
-          return true if match_rule(choice)
-          assert_pos(point.pos)
-          return false if point.cut
+    def rep(range = 0.., &block)
+      range = range..range if Integer === range
+      range = 0..range.max if range.begin.nil?
+      unless Integer === range.min && (range.end.nil? || Integer === range.max)
+        raise "Range malformed #{range}"
+      end
+      _bt do
+        max = range.end && range.max
+        ar = []
+        (1..max).each do
+          res = _bt(&block)
+          break unless res
+          ar << res
         end
+        (ar.size >= range.min) ? ar : fail!
       end
-      false
     end
 
-    def match_opt(rule)
-      @choice_stack.with do |point|
-        return true if match_rule(rule)
-        assert_pos(point.pos)
-        return false if point.cut
-      end
-      true
+    def text(&block)
+      pos = @scan.pos
+      _bt(&block) && @str.byteslice(pos, @scan.pos - pos)
     end
 
-    def match_rep(range, seq)
-      raise "Range malformed #{range}" unless Integer === r.min && (r.end.nil? || Integer === r.max)
-      @choice_stack.with do |point|
-        arr = (1..range.min).map do
-          save_pos = @scan.pos
-          unless match_rule(seq)
-            assert_pos(save_pos)
-            @scan.pos = point.pos
-            return false
-          end
-          @result
-        end
-        (range.min..(range.end && range.max)).each do
-          point.cut = false
-          save_pos = @scan.pos
-          unless match_rule(seq)
-            assert_pos(save_pos)
-            if point.cut
-              @scan.pos = point.pos
-              return false
-            end
-            return true
-          end
-          arr << @result
-        end
+    def will?(&block)
+      _rewind(@scan.pos, false, _bt(&block))
+    end
+
+    def wont!(&block)
+      _rewind(@scan.pos, @failed, !_bt(&block)) || fail!
+    end
+
+    # cut point handling
+    #   cut do
+    #     seq { lit("{") && cut! && lit("}") } ||
+    #     !cut? && seq { lit("[") && cut! && lit("]") } ||
+    #     !cut? && dot
+    #   end
+    class CutPoint
+      attr_accessor :next
+
+      def initialize
+        @cut = false
+        @next = nil
       end
-      @result = arr
-      true
+
+      def cut!
+        @next&.cut!
+        @cut = true
+      end
+
+      def cut?
+        @cut
+      end
+    end
+
+    # for use with cut! and cut?
+    def cut
+      prev_cut = @cut
+      @cut = CutPoint.new
+      prev_cut.next = @cut
+      yield @cut
+    ensure
+      prev_cut.next = nil
+      @cut = prev_cut
     end
 
     def cut!
-      if @choice_stack.cut!
-        # @memoize.clear
-      end
+      @cut.cut!
     end
 
-    def match_sequence(sequence)
-      save_pos = @scan.pos
-      sequence.each do |seq|
-        spos = @scan.pos
-        unless match_rule(seq)
-          assert_pos(spos)
-          @scan.pos = save_pos
-          return false
-        end
+    def cut?
+      @cut.cut?
+    end
+
+    # Position handling for failures
+
+    Position = Struct.new(:lineno, :colno, :line, :charpos)
+
+    private
+
+    def init_line_ends
+      @line_ends = [-1]
+      pos = 0
+      while (pos = @str.index("\n", pos))
+        @line_ends << @pos
+        pos += 1
       end
-      true
+      @line_ends << @str.size
+    end
+
+    public
+
+    def position_for_charpos(charpos)
+      lineno = @line_ends.bsearch_index { |x| x >= charpos }
+      case lineno
+      when nil
+        raise "Position #{charpos} is larger than string size #{@str.size}"
+      else
+        prev_end = @line_ends[lineno - 1]
+        line_start = prev_end + 1
+        column = charpos - prev_end
+      end
+      line = @str[line_start..@line_ends[lineno]]
+      Position.new(lineno, column, line, charpos)
+    end
+
+    # helper methods
+
+    private
+
+    def _bt
+      pos = @scan.pos
+      res = yield
+      if res
+        @failed = nil if @failed && @failed.bytepos <= @scan.pos
+        res
+      else
+        @scan.pos = pos
+        nil
+      end
+    rescue
+      @scan.pos = pos
+      raise
+    end
+
+    def _rewind(pos, failed, val)
+      @scan.pos = pos if pos
+      @failed = failed if failed != false
+      val
     end
   end
 end
--- spec/parser_spec.rb
+++ spec/parser_spec.rb
@@ -2,178 +2,286 @@
 
 RSpec.describe FuPeg::Parser do
   it "could be created" do
-    expect{ FuPeg::Parser.new("hello") }.not_to raise_error
+    expect { FuPeg::Parser.new("hello") }.not_to raise_error
   end
 
-  it "should match dot" do
-    parser = FuPeg::Parser.new("hello")
-    class << parser
-      def root
-        match_dot
+  context "simple actions" do
+    let(:parser) { FuPeg::Parser.new("hello") }
+    it "should match dot" do
+      class << parser
+        def root
+          dot
+        end
       end
+      expect(parser.root).to be_truthy
+      expect(parser.charpos).to eq 1
     end
-    expect(parser.parse(:root)).to be true
-    expect(parser.charpos).to eq 1
-  end
 
-  it "should match string" do
-    parser = FuPeg::Parser.new("hello")
-    class << parser
-      def root
-        match_lit('hell')
+    it "should match string" do
+      class << parser
+        def root
+          lit("hell")
+        end
       end
+      expect(parser.root).to be_truthy
+      expect(parser.charpos).to eq 4
     end
-    expect(parser.parse(:root)).to be true
-    expect(parser.charpos).to eq 4
-  end
 
-  it "should detect not matched regexp" do
-    parser = FuPeg::Parser.new("hello")
-    class << parser
-      def root
-        match_lit('helo')
+    it "should fail string" do
+      class << parser
+        def root
+          lit("helo")
+        end
       end
+      expect(parser.root).to be_falsey
+      expect(parser.charpos).to eq 0
+      expect(parser.failed.pos.charpos).to eq 0
     end
-    expect(parser.parse(:root)).to be_falsey
-    expect(parser.charpos).to eq 0
-  end
 
-  it "should match regexp" do
-    parser = FuPeg::Parser.new("hello")
-    class << parser
-      def root
-        match_lit(/he?l*/)
+    it "should match regexp" do
+      class << parser
+        def root
+          lit(/he?l*/)
+        end
       end
+      expect(parser.root).to be_truthy
+      expect(parser.charpos).to eq 4
     end
-    expect(parser.parse(:root)).to be true
-    expect(parser.charpos).to eq 4
-  end
 
-  it "should detect not matched regexp" do
-    parser = FuPeg::Parser.new("hello")
-    class << parser
-      def root
-        match_lit(/h?el+a/)
+    it "should fail regexp" do
+      class << parser
+        def root
+          lit(/h?el+a/)
+        end
       end
+      expect(parser.root).to be_falsey
+      expect(parser.charpos).to eq 0
+      expect(parser.failed.pos.charpos).to eq 0
     end
-    expect(parser.parse(:root)).to be_falsey
-    expect(parser.charpos).to eq 0
-  end
 
-  it "should match choice 1" do
-    parser = FuPeg::Parser.new("hello")
-    class << parser
-      def root
-        match_choices([
-          [:match_lit, "hell"],
-          [:match_lit, "sky"]
-        ])
+    it "should match choice" do
+      class << parser
+        def root
+          lit("hell") || lit("sky")
+        end
       end
+      expect(parser.root).to be_truthy
+      expect(parser.charpos).to eq 4
     end
-    expect(parser.parse(:root)).to be true
-    expect(parser.charpos).to eq 4
-  end
 
-  it "should match choice 2" do
-    parser = FuPeg::Parser.new("hello")
-    class << parser
-      def root
-        match_choices([
-          ->{ match_lit("sky") },
-          ->{ match_lit("hell") },
-        ])
+    it "should match choice 2" do
+      class << parser
+        def root
+          lit("sky") || lit("hell")
+        end
       end
+      expect(parser.root).to be_truthy
+      expect(parser.charpos).to eq 4
     end
-    expect(parser.parse(:root)).to be true
-    expect(parser.charpos).to eq 4
-  end
 
-  it "should match sequence" do
-    parser = FuPeg::Parser.new("hello")
-    class << parser
-      def root
-        match_sequence([
-          ->{ match_lit(/[he]*/) },
-          [:match_lit, "ll"]
-        ])
+    it "should fail choice" do
+      class << parser
+        def root
+          lit("sky") || lit("halo")
+        end
       end
+      expect(parser.root).to be_falsey
+      expect(parser.charpos).to eq 0
+      expect(parser.failed.pos.charpos).to eq 0
     end
-    expect(parser.parse(:root)).to be true
-    expect(parser.charpos).to eq 4
-  end
 
-  it "should match sequence (simple)" do
-    parser = FuPeg::Parser.new("hello")
-    class << parser
-      def root
-        match_lit(/[he]*/) &&
-        match_lit("ll")
+    it "should match sequence" do
+      class << parser
+        def root
+          seq { lit(/[he]*/) && lit("ll") }
+        end
       end
+      expect(parser.root).to be_truthy
+      expect(parser.charpos).to eq 4
     end
-    expect(parser.parse(:root)).to be true
-    expect(parser.charpos).to eq 4
-  end
 
-  it "should detect not matched sequence 1" do
-    parser = FuPeg::Parser.new("hello")
-    class << parser
-      def root
-        match_sequence([
-          ->{ match_lit(/[ho]{2}/) },
-          [:match_lit, "ll"]
-        ])
+    it "should fail sequence 1" do
+      class << parser
+        def root
+          seq { lit(/[ho]{2}/) && lit("ll") }
+        end
       end
+      expect(parser.root).to be_falsey
+      expect(parser.charpos).to eq 0
+      expect(parser.failed.pos.charpos).to eq 0
     end
-    expect(parser.parse(:root)).to be_falsey
-    expect(parser.charpos).to eq 0
-  end
 
-  it "should detect not matched sequence 2" do
-    parser = FuPeg::Parser.new("hello")
-    class << parser
-      def root
-        match_sequence([
-          ->{ match_lit(/[he]{2}/) },
-          [:match_lit, "la"]
-        ])
+    it "should fail sequence 2" do
+      class << parser
+        def root
+          seq { lit(/[he]{2}/) && lit("la") }
+        end
       end
+      expect(parser.root).to be_falsey
+      expect(parser.charpos).to eq 0
+      expect(parser.failed.pos.charpos).to eq 2
     end
-    expect(parser.parse(:root)).to be_falsey
-    expect(parser.charpos).to eq 0
-  end
 
-  it "should detect not matched sequence 1 (simple)" do
-    parser = FuPeg::Parser.new("hello")
-    class << parser
-      def root
-        match_lit(/[ho]{2}/) &&
-        match_lit("ll")
+    it "should match optional" do
+      class << parser
+        def root
+          opt { lit(/[he]*/) }
+        end
       end
+      expect(parser.root).to be_truthy
+      expect(parser.charpos).to eq 2
+      expect(parser.failed).to be_nil
     end
-    expect(parser.parse(:root)).to be_falsey
-    expect(parser.charpos).to eq 0
-  end
 
-  it "should detect not matched sequence 2 (simple)" do
-    parser = FuPeg::Parser.new("hello")
-    class << parser
-      def root
-        match_lit(/[he]{2}/) &&
-        match_lit("la")
+    it "should match optional 2" do
+      class << parser
+        def root
+          opt { lit(/[va]+/) }
+        end
       end
+      expect(parser.root).to be_truthy
+      expect(parser.charpos).to eq 0
+      expect(parser.failed).to be_nil
     end
-    expect(parser.parse(:root)).to be_falsey
-    expect(parser.charpos).to eq 0
-  end
 
-  it "should match optional" do
-    parser = FuPeg::Parser.new("hello")
-    class << parser
-      def root
-        match_opt([:match_lit, /[he]*/])
+    it "should match look ahead" do
+      class << parser
+        def root
+          seq {
+            will? { lit(/[he]+/) } &&
+              lit("hell")
+          }
+        end
       end
+      expect(parser.root).to be_truthy
+      expect(parser.charpos).to eq 4
+      expect(parser.failed).to be_nil
     end
-    expect(parser.parse(:root)).to be true
-    expect(parser.charpos).to eq 2
+
+    it "should fail look ahead" do
+      class << parser
+        def root
+          seq {
+            will? { lit(/[va]+/) } &&
+              lit("hell")
+          }
+        end
+      end
+      expect(parser.root).to be_falsey
+      expect(parser.charpos).to eq 0
+      expect(parser.failed).to_not be_nil
+    end
+
+    it "should match negation" do
+      class << parser
+        def root
+          seq {
+            wont! { lit(/[va]+/) } &&
+              lit("hell")
+          }
+        end
+      end
+      expect(parser.root).to be_truthy
+      expect(parser.charpos).to eq 4
+      expect(parser.failed).to be_nil
+    end
+
+    it "should fail negation" do
+      class << parser
+        def root
+          seq {
+            wont! { lit(/[he]+/) } &&
+              lit("hell")
+          }
+        end
+      end
+      expect(parser.root).to be_falsey
+      expect(parser.charpos).to eq 0
+      expect(parser.failed).to_not be_nil
+    end
+
+    it "should handle 0.. repetition" do
+      class << parser
+        def root
+          rep { lit(/[helo]/) }
+        end
+      end
+
+      expect(parser.root).to eq %w[h e l l o]
+      expect(parser.charpos).to eq 5
+      expect(parser.failed).to be_nil
+    end
+
+    it "should handle 0.. repetition 1" do
+      class << parser
+        def root
+          rep { lit(/[zelo]/) }
+        end
+      end
+
+      expect(parser.root).to eq %w[]
+      expect(parser.charpos).to eq 0
+      expect(parser.failed).to be_nil
+    end
+
+    it "should handle 1.. repetition" do
+      class << parser
+        def root
+          rep(1..) { lit(/[helo]/) }
+        end
+      end
+
+      expect(parser.root).to eq %w[h e l l o]
+      expect(parser.charpos).to eq 5
+      expect(parser.failed).to be_nil
+    end
+
+    it "should handle 1.. repetition 1" do
+      class << parser
+        def root
+          rep(1..) { lit(/[hely]/) }
+        end
+      end
+
+      expect(parser.root).to eq %w[h e l l]
+      expect(parser.charpos).to eq 4
+      expect(parser.failed).to be_nil
+    end
+
+    it "should fail 1.. repetition" do
+      class << parser
+        def root
+          rep(1..) { lit(/[zelo]/) }
+        end
+      end
+
+      expect(parser.root).to be_falsey
+      expect(parser.charpos).to eq 0
+      expect(parser.failed).to_not be_nil
+      parser.report_failed($stderr)
+    end
+
+    it "should handle 1..3 repetition" do
+      class << parser
+        def root
+          rep(1..3) { lit(/[helo]/) }
+        end
+      end
+
+      expect(parser.root).to eq %w[h e l]
+      expect(parser.charpos).to eq 3
+      expect(parser.failed).to be_nil
+    end
+
+    it "should capture text" do
+      class << parser
+        def root
+          seq { dot && (x = text { rep(3) { dot } }) && dot && x }
+        end
+      end
+      expect(parser.root).to eq "ell"
+      expect(parser.charpos).to eq 5
+      expect(parser.failed).to be_nil
+    end
   end
 end
